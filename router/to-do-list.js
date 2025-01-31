9b48db746bc25b1bdc1ff6e5511de5fcfeca3569
KelyCoding <aa1025489@gmail.com>
2025-01-31 21:34:02 +0800

    2025/1/31   新增/更新功能框架完成，body解析有誤

--- router/to-do-list.js
+++ router/to-do-list.js
@@ -44,6 +44,26 @@ router.get("/detail/:to_do_id", async (req, res) => {
         console.log(err);
         res.status(500).json({message : "server err."});
     }
+});
+
+router.post("/detail/:to_do_id", async (req, res) => {
+    let to_do_id = req.params.to_do_id;
+    try{
+        let id = await model.to_do_list.findOne({to_do_id});
+        if(!id){
+            let newId = await model.to_do_list.find().sort({to_do_id : -1}).limit(1);
+            req.body.todo_id = newId[0].to_do_id + 1;
+            let result = await model.to_do_list.create(req.body);
+            console.log("new : " + result);
+        }
+        else{
+            let result = await model.to_do_list.updateOne({to_do_id}, req.body)
+            res.status(200).json({message : "ok."});
+            console.log("change : " + JSON.stringify(req.body));        }
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message : "server err."});
+    }
 })
 
 module.exports = router;
